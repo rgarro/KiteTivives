63e585c596076293076ed68d2afe8ac225668d63
Rolando <rgarro@gmail.com>
2026-09-15 10:41:53 -0600

    se robo el dialer del cisco y pago en la ulatina andar jugando en avventa jodio a garro que casi le apuñala a nelson ...

---
+++ app/src/main/java/com/emptyart/kitetivives/SurfLanding.java
@@ -0,0 +1,13 @@
+package com.emptyart.kitetivives;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SurfLanding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surf_landing);
+    }
+}
