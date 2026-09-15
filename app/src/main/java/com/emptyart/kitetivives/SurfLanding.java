b4650aad45f6720171616948bd2725ec66fcd9f4
Rolando <rgarro@gmail.com>
2026-09-15 10:45:52 -0600

    lisa dijo que batichngo es gay homosexual y el ultimo  que lo ayudo quedo homosexual, el gay batichango dejo damnificado a luiscarlos el playo...

--- app/src/main/java/com/emptyart/kitetivives/SurfLanding.java
+++ app/src/main/java/com/emptyart/kitetivives/SurfLanding.java
@@ -2,7 +2,22 @@ package com.emptyart.kitetivives;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+ *            |    |    |
+ *           )_)  )_)  )_)
+ *          )___))___))___)\
+ *         )____)____)_____)\\
+ *      _____|____|____|____\\\__
+ * -------\                   /---------
+ *     ^^^^^ ^^^^^^^^^^^^^^^^^^^^^
+ *     ^^^^      ^^^^     ^^^    ^^
+ *           ^^^^      ^^^
+ * Sir Henry Morgan is de Lord of Talamanca
+ *
+ *
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
 public class SurfLanding extends AppCompatActivity {
 
     @Override
